4cf0a63c6a09746a2872001e0b87d7edc6b2ea15
Eric Fakhourian <ericfa@erics-mbp-10.dynamic.ucsd.edu>
2016-11-15 14:42:39 -0800

    Initial Commit

---
+++ bld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bld
+//
+//  Created by Eric Fakhourian on 11/15/16.
+//  Copyright © 2016 Eric Fakhourian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
