2ba2c7af62d1ba34ec1003daf312ed0c8bf72d8b
Ben Perez <benperez1227@gmail.com>
2016-12-03 12:09:31 -0800

    changed calendar data type and day data type. Also added view controllers for calendar functionality

---
+++ MealViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MealViewController.swift
+//  bld
+//
+//  Created by Benjamin Perez on 12/2/16.
+//  Copyright © 2016 Eric Fakhourian. All rights reserved.
+//
+
+import UIKit
+
+class MealViewController: UIViewController {
+    
+    var recipe: Recipe?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
